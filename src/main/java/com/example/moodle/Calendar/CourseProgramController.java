2380c2458f73333f44e257d9e556d8137c725730
hemery mbong <hemerymbong8@gmail.com>
2024-06-20 19:47:53 +0100

    version hibernate

--- src/main/java/com/example/moodle/Calendar/CourseProgramController.java
+++ src/main/java/com/example/moodle/Calendar/CourseProgramController.java
@@ -34,16 +34,7 @@ public class CourseProgramController {
         String courseDuration = courseDurationField.getText();
         String dateString = courseDateField.getText();
 
-        /*try {
-            ZonedDateTime courseDate = ZonedDateTime.parse(dateString + "T00:00:00" + calendarController.getDateFocus().getOffset());
-            CalendarActivity newActivity = new CalendarActivity(courseDate, courseName, Integer.parseInt(courseDuration));
-            calendarController.addCourse(newActivity);
-            Stage stage = (Stage) courseNameField.getScene().getWindow();
-            stage.close();
-        } catch (DateTimeParseException e) {
-            // Handle error: invalid date format
-            e.printStackTrace();
-        }*/
+
     }
 
     @FXML
---
+++ src/main/java/com/example/moodle/model/Teacher.java
@@ -0,0 +1,92 @@
+package com.example.moodle.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Teacher")
+public class Teacher implements java.io.Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "statut")
+    private String statut;
+
+    public Teacher(Long id, String name, String surname, String username, String password, String email, String statut) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.statut = statut;
+    }
+
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+}
