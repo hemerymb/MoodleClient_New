18f9020bd6b9704fb10fd0b9b54a10c72984940b
El-Prof-ai <afoueguimdouanla@gmail.com>
2024-06-20 18:41:49 +0100

    Les interfaces sur les devoirs

---
+++ src/main/java/com/example/moodle/Student/studentAssigmentPanel/StudentAssigmentViewPanelController.java
@@ -0,0 +1,60 @@
+package com.example.moodle.Student.studentAssigmentPanel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+
+public class StudentAssigmentViewPanelController {
+
+    @FXML
+    private Button AssignmentsBtn;
+
+    @FXML
+    private Button ChaptersBtn;
+
+    @FXML
+    private Button CourseFilesBtn;
+
+    @FXML
+    private Button ParticipantsBtn;
+
+    @FXML
+    private TextArea courseDescription;
+
+    @FXML
+    private Label coursename;
+
+    @FXML
+    private GridPane gridpane;
+
+    @FXML
+    private Label returnArrow;
+
+    @FXML
+    private ScrollPane scrollpane;
+
+    @FXML
+    void handleAssignmentsBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleChaptersBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCourseFilesBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleParticipantsBtn(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/moodle/Student/studentAssigmentPanel/studentAssigmentCardController.java
@@ -0,0 +1,22 @@
+package com.example.moodle.Student.studentAssigmentPanel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class studentAssigmentCardController {
+
+    @FXML
+    private Label courseDesc;
+
+    @FXML
+    private Label courseName;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}
