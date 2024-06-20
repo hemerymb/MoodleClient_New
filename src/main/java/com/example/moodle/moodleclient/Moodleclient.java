5d67bf3c59a96396d2c390f3cfd4eb3560438690
Electrobolt-temp <165054062+Electrobolt-temp@users.noreply.github.com>
2024-06-20 13:24:53 +0100

    Mise à jour mineure

---
+++ src/main/java/com/example/moodle/moodleclient/Moodleclient.java
@@ -0,0 +1,16 @@
+package com.example.moodle.moodleclient;
+
+import javafx.scene.layout.BorderPane;
+
+public class Moodleclient {
+    public static BorderPane root;
+    public static client_moodle client;
+
+}
+
+class client_moodle {
+    String name;
+    String surname;
+    String username;
+    String status;
+}
