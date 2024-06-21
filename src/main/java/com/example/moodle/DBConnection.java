ee72f494d1a5c02635b72ad62ee0640442a89736
hemery mbong <hemerymbong8@gmail.com>
2024-06-21 19:45:10 +0100

    Update with mh2

--- src/main/java/com/example/moodle/DBConnection.java
+++ src/main/java/com/example/moodle/DBConnection.java
@@ -8,6 +8,7 @@ public class DBConnection {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/moodleclient";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
