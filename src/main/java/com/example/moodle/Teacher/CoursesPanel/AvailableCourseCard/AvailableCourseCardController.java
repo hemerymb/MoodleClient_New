76759d00e52cb2e9925a19aa39f6992324ceaf78
hemery mbong <hemerymbong8@gmail.com>
2024-06-21 07:54:53 +0100

    connexion course et users avec la BD

--- src/main/java/com/example/moodle/Teacher/CoursesPanel/AvailableCourseCard/AvailableCourseCardController.java
+++ src/main/java/com/example/moodle/Teacher/CoursesPanel/AvailableCourseCard/AvailableCourseCardController.java
@@ -22,20 +22,23 @@ public class AvailableCourseCardController implements Initializable{
     private Label courseName;
 
     @FXML
-    private Label ChapterNumber;
+    private Label ChaptersNumber;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
     }
 
     @FXML
     void handleCourseView(MouseEvent event) throws IOException {
-
         FXMLLoader contentLoader = new FXMLLoader(AvailableCourseCardController.class.getResource("/com/example/moodle/FXML/CourseViewPanel_updated.fxml"));
         AnchorPane content = contentLoader.load();
         root.setCenter(content);
-
     }
 
+    public void setCourseDetails(String name, String description, int chapters) {
+        this.courseName.setText(name);
+        this.courseDesc.setText(description);
+        this.ChaptersNumber.setText(String.valueOf(chapters) + " Chapitres");
+    }
 }
