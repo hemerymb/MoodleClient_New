df4fa434a3d937c731ede03270699241cfdbdb53
[MH] <[mbailassem1st@gmail.com]>
2024-06-21 13:48:29 +0100

    Merging des branches hemery et mh2

--- src/main/java/com/example/moodle/Teacher/CoursesPanel/AvailableCourseCard/AvailableCourseCardController.java
+++ src/main/java/com/example/moodle/Teacher/CoursesPanel/AvailableCourseCard/AvailableCourseCardController.java
@@ -1,11 +1,17 @@
 package com.example.moodle.Teacher.CoursesPanel.AvailableCourseCard;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import static com.example.moodle.moodleclient.Moodleclient.root;
 
 public class AvailableCourseCardController implements Initializable{
 
@@ -30,6 +36,6 @@ public class AvailableCourseCardController implements Initializable{
     public void setCourseDetails(String name, String description, int chapters) {
         this.courseName.setText(name);
         this.courseDesc.setText(description);
-        this.ChaptersNumber.setText(String.valueOf(chapters) + " Chapitres");
+        // this.ChaptersNumber.setText(String.valueOf(chapters) + " Chapitres");
     }
 }
