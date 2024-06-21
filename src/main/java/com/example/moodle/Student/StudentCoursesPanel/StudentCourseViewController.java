31e80a77ff793e3c1bdb1b0bc51c6c78aec4f34f
[MH] <[mbailassem1st@gmail.com]>
2024-06-21 10:06:07 +0100

    Génération des cartes de cours

---
+++ src/main/java/com/example/moodle/Student/StudentCoursesPanel/StudentCourseViewController.java
@@ -0,0 +1,4 @@
+package com.example.moodle.Student.StudentCoursesPanel;
+
+public class StudentCourseViewController {
+}
--- src/main/java/com/example/moodle/Teacher/CoursesPanel/AvailableCourseCard/AvailableCourseCardController.java
+++ src/main/java/com/example/moodle/Teacher/CoursesPanel/AvailableCourseCard/AvailableCourseCardController.java
@@ -6,8 +6,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 public class AvailableCourseCardController implements Initializable{
+    @FXML
+    private Label chaptersNumber;
 
     @FXML
     private Label courseDesc;
@@ -15,9 +19,24 @@ public class AvailableCourseCardController implements Initializable{
     @FXML
     private Label courseName;
 
+    @FXML
+    private Pane pane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+    }
+
+    public void define(String courseName, String description, int nbChapters) {
+        this.courseName.setText(courseName);
+        this.courseDesc.setText(description);
+        this.courseDesc.setStyle("-fx-text-fill: black");
+        this.chaptersNumber.setText(nbChapters + " Chapitres");
+        this.chaptersNumber.setStyle("-fx-text-fill: black");
+    }
+
+    @FXML
+    void handleCourse(MouseEvent event) {
+        System.out.println("Yo");
     }
 
 }
