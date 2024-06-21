5f786a93fd7a215e8c4154d5ce6fc440aa1c8c12
[MH] <[mbailassem1st@gmail.com]>
2024-06-21 17:59:30 +0100

    Ajout du fichier SQL

---
+++ src/main/resources/com/example/moodle/SQL/moodleclient.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS moodleclient;
+USE moodleclient;
+
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    username VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    statut VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Course (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    courseName VARCHAR(255),
+    courseAbr VARCHAR(50),
+    courseDescription TEXT,
+    nbChapters INT,
+    nbAssignments INT,
+    teacherId INT,
+    FOREIGN KEY (teacherId) REFERENCES Users(id)
+);
+
+CREATE TABLE IF NOT EXISTS Chapters (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    num INT,
+    content VARCHAR(255),
+    courseId INT,
+    FOREIGN KEY (courseId) REFERENCES Users(id)
+);
+
+CREATE TABLE IF NOT EXISTS assignments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    assignmentName VARCHAR(255) NOT NULL,
+    createdDate DATE NOT NULL,
+    limitedDate DATE NOT NULL,
+    courseName VARCHAR(255),
+    statut ENUM('in progress', 'finish') NOT NULL
+);
+CREATE TABLE IF NOT EXISTS private_files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fileName VARCHAR(255) NOT NULL,
+    fileSize BIGINT NOT NULL,
+    fileType VARCHAR(50) NOT NULL,
+    filePath VARCHAR(255) NOT NULL
+);
+
