9c626774849231543bd651426fdd55a5ae8ec6ce
hemery mbong <hemerymbong8@gmail.com>
2024-06-22 08:59:19 +0100

    Final version

--- src/main/resources/com/example/moodle/SQL/moodleclient.sql
+++ src/main/resources/com/example/moodle/SQL/moodleclient.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS Users (
     username VARCHAR(255),
     password VARCHAR(255),
     email VARCHAR(255),
-    statut VARCHAR(255)
+    statut VARCHAR(255),
+    syncedbyte int default 0
 );
 
 CREATE TABLE IF NOT EXISTS Course (
@@ -19,7 +20,8 @@ CREATE TABLE IF NOT EXISTS Course (
     nbChapters INT,
     nbAssignments INT,
     teacherId INT,
-    FOREIGN KEY (teacherId) REFERENCES Users(id)
+    FOREIGN KEY (teacherId) REFERENCES Users(id),
+    syncedbyte int default 0
 );
 
 CREATE TABLE IF NOT EXISTS Chapters (
@@ -28,7 +30,8 @@ CREATE TABLE IF NOT EXISTS Chapters (
     num INT,
     content VARCHAR(255),
     courseId INT,
-    FOREIGN KEY (courseId) REFERENCES Course(id)
+    FOREIGN KEY (courseId) REFERENCES Users(id),
+    syncedbyte int default 0
 );
 
 CREATE TABLE IF NOT EXISTS assignments (
@@ -37,13 +40,15 @@ CREATE TABLE IF NOT EXISTS assignments (
     createdDate DATE NOT NULL,
     limitedDate DATE NOT NULL,
     courseName VARCHAR(255),
-    statut ENUM('in progress', 'finish') NOT NULL
+    statut ENUM('in progress', 'finish') NOT NULL,
+    syncedbyte int default 0
 );
 CREATE TABLE IF NOT EXISTS private_files (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fileName VARCHAR(255) NOT NULL,
     fileSize BIGINT NOT NULL,
     fileType VARCHAR(50) NOT NULL,
-    filePath VARCHAR(255) NOT NULL
+    filePath VARCHAR(255) NOT NULL,
+    syncedbyte int default 0
 );
 
